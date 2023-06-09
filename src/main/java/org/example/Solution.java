d711bb21524ca3601a41f5fa44c5eb665dc08528
alek <wait82985@gmail.com>
2023-06-09 12:22:01 +0600

    initial commit. 2 test cases

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Solution {
+    public int trap(int[] height) {
+        //TODO
+        return 0;
+
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,19 @@
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void trapTest1() {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int output = 6;
+        Assert.assertEquals(output, new Solution().trap(height));
+    }
+
+    @Test
+    public void trapTest2() {
+        int[] height = {4, 2, 0, 3, 2, 5};
+        int output = 9;
+        Assert.assertEquals(output, new Solution().trap(height));
+    }
+}
